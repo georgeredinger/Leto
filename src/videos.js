cd588c0a8c080728f5e63f17f974f670dac11ac9
george redinger <george@georgeredinger.com>
2017-04-30 13:08:11 -0700

    youtube api callbacks must be in global scope

---
+++ src/videos.js
@@ -0,0 +1,86 @@
+
+export const vid = [{
+  'videoId': 'S7nOviZaMO0',
+  'startSeconds': 180,
+  'endSeconds': 190,
+  'suggestedQuality': 'hd720',
+  'message': 'first'
+},
+{
+  'videoId': 'wspUEkFo9oI',
+  'startSeconds': 120,
+  'endSeconds': 127,
+  'suggestedQuality': 'hd720',
+  'message': 'Wait, that\'s not Leto'
+
+},
+{
+  'videoId': 'PZpYZ3xYfQE',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto Transitions'
+},
+{
+  'videoId': 's9cgFPB-lD4',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto at Spokane Sport Horse'
+
+},
+{
+  'videoId': 'SAm34wutr2w',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto winning at SSH'
+
+},
+{
+  'videoId': 'v2dzXbJhz9k',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto at home Winter'
+
+},
+{
+  'videoId': 'HSZONtn0m8U',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto at home Summer'
+
+},
+{
+  'videoId': '_SBfBp6oN-Y',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Gail\'s goof'
+
+},
+{
+  'videoId': 'CCWxicZFaVU',
+  'startSeconds': 120,
+  'endSeconds': 141,
+  'suggestedQuality': 'hd720',
+  'message': 'Leto cool'
+
+}
+];
+
+
+export const playerDefaults = {
+  autoplay: 0,
+  autohide: 1,
+  modestbranding: 0,
+  rel: 0,
+  showinfo: 0,
+  controls: 0,
+  disablekb: 1,
+  enablejsapi: 1,
+  iv_load_policy: 3,
+  videoId: vid[0]
+};
